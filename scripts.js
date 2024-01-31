88f57d86737975d293b0f539d7bc6f5f50035713
RaviCarrillo <ravicarrillo@gmail.com>
2024-01-31 11:23:22 -0300

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -6,4 +6,10 @@ $(document).ready(function () {
 
 function toggleCardRotation(card) {
     card.querySelector('.card-inner').classList.toggle('flipped');
-}
+};
+
+document.querySelectorAll('.card').forEach(card => {
+    card.addEventListener('click', () => {
+      card.classList.toggle('is-flipped');
+    });
+  });
